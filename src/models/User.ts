39f83736bc395feb8e1c85f21af3de46aac57e3b
d33panpaudel07 <dodeepan7@gmail.com>
2024-09-07 09:09:40 +0545

    done

--- src/models/User.ts
+++ src/models/User.ts
@@ -46,7 +46,7 @@ const userSchema = new mongoose.Schema(
         mood: Number,
         moodDuration: Number,
         stressLevel: Number,
-        anxietySymptoms: [String],
+        anxietySymptoms: [{type:String}],
         sleepQuality: Number,
         sleepHours: Number,
         sleepDisturbance: String,
