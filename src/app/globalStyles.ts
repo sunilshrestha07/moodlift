1cd65d20e100bc479cb0dd824ddcabeadc758e37
Samir1611 <123559550+Samir1611@users.noreply.github.com>
2024-09-07 09:56:32 +0545

    updated layout

---
+++ src/app/globalStyles.ts
@@ -0,0 +1,3 @@
+export const pagePadding = `px-[2vw] md:px-[5vw] xl:px-[10vw] 3xl:px-[15vw]`;
+export const navbarPadding = `px-[2vw] md:px-[3vw] xl:px-[7vw] 3xl:px-[12vw]`;
+export const sectionPadding = `px-[2vw] md:px-[3vw] xl:px-[7vw] 3xl:px-[12vw]`;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,31 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Poppins } from "next/font/google";
+import Navbar from "../components/Navbar";
+import Section from "@/components/Section";
 
 const poppins = Poppins({
-    subsets: ["latin"],
-    weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
-    title: "Moodlift",
-    description: "Mental health tracking app Enhanced by AI and ",
+  title: "Moodlift",
+  description: "Mental health tracking app Enhanced by AI and ",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body className={`${poppins.className}`}>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={`${poppins.className}`}>
+        <Navbar />
+        <Section />
+        {children}
+      </body>
+    </html>
+  );
 }
