781fa4e6bde41cd4fc24d36fcd1e0bff9d91bb99
d33panpaudel07 <dodeepan7@gmail.com>
2024-09-07 10:55:52 +0545

    dev/deleteAllUsers and /[id]/updateUser done

--- src/app/api/dev/[id]/getUser/route.ts
+++ src/app/api/dev/[id]/getUser/route.ts
@@ -2,7 +2,6 @@ import dbConnect from "@/lib/db";
 import User from "@/models/User";
 import { NextRequest, NextResponse } from "next/server";
 
-<<<<<<< HEAD
 //TESTING DONE
 
 export async function GET(
@@ -11,42 +10,13 @@ export async function GET(
 ) {
   try {
     await dbConnect();
-=======
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { userid: string } }
-) {
-  try {
-    await dbConnect();
-
-    // Log the params to see what we're receiving
-    console.log("Received params:", params);
-
-    const userId = params.userid;
-    if (!userId) {
-      return NextResponse.json(
-        { message: "userId not provided" },
-        { status: 400 }
-      );
-    }
-
-    console.log("Attempting to find user with ID:", userId);
-
-    const user = await User.findById(userId);
-    if (!user) {
-      return NextResponse.json({ message: "User not found" }, { status: 404 });
-    }
-
-    return NextResponse.json(user, { status: 200 });
->>>>>>> d2f87729eb75ed7bfc9a718418c5b9d7ae756acf
   } catch (error) {
-    console.error("Error in GET /api/[userId]/getUser:", error);
+    console.error("Error connecting to database", error);
     return NextResponse.json(
-      { message: "Internal server error" },
+      { message: "DB connection error" },
       { status: 500 }
     );
   }
-<<<<<<< HEAD
   //displaying received parameters
   // console.log("Received params:", params);
 
@@ -66,6 +36,4 @@ export async function GET(
   }
 
   return NextResponse.json(user, { status: 200 });
-=======
->>>>>>> d2f87729eb75ed7bfc9a718418c5b9d7ae756acf
 }
