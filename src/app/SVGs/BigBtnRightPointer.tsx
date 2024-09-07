c808add1b147d9660fd2b88cbd0d9be1ced1313d
Biplove <biplovegautam123@gmail.com>
2024-09-07 18:34:49 +0545

    commit before ai implementation

--- src/app/SVGs/BigBtnRightPointer.tsx
+++ src/app/SVGs/BigBtnRightPointer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { svgProp } from "../types";
 
 const BigBtnRightPointer = (prop: svgProp) => {
     return (
--- src/app/SVGs/RightPointer.tsx
+++ src/app/SVGs/RightPointer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { svgProp } from "../types";
 
 const RightPointer = (prop: svgProp) => {
     return (
