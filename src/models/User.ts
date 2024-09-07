633e5b9a52382d60965166708c7f64bb5f45e032
d33panpaudel07 <dodeepan7@gmail.com>
2024-09-07 16:39:36 +0545

    basicUserOperationsOnlyByUser

--- src/models/User.ts
+++ src/models/User.ts
@@ -59,64 +59,7 @@ const userSchema = new mongoose.Schema(
             },
         ],
     },
-<<<<<<< HEAD
-    gender: {
-      type: String,
-    },
-    age: {
-      type: Number,
-    },
-    pastExperiences: {
-      type: Boolean,
-    },
-    previousDiagnosis: [
-      {
-        type: String,
-      },
-    ],
-    goal: {
-      type: String,
-    },
-    phone: {
-      type: String,
-    },
-    avatar: {
-      type: String,
-    },
-    email: {
-      type: String,
-    },
-    posts: [
-      {
-        type: String,
-      },
-    ],
-    noOfPosts: {
-      type: Number,
-    },
-    activityInfo: [
-      {
-        date: { type: Date, default: Date.now() },
-        mood: Number,
-        moodDuration: Number,
-        stressLevel: Number,
-        anxietySymptoms: [{type:String}],
-        sleepQuality: Number,
-        sleepHours: Number,
-        sleepDisturbance: String,
-        energyLevel: Number,
-        physicalActivity: String,
-        eatingHabit: String,
-        socialEnjoyment: Number,
-        loneliness: Number,
-        currentTreatment: String,
-      },
-    ],
-  },
-  { timestamps: true }
-=======
     { timestamps: true }
->>>>>>> 491155fe1082ee188f44833ad285dbe63cf98259
 );
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
