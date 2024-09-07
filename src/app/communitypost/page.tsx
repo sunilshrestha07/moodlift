5d1099434ea1b2bcbf74ed307b1c830217ac0d4f
Your Name <sunilcrestha9@gmai.com>
2024-09-07 10:03:36 +0545

    some changes

---
+++ src/app/communitypost/page.tsx
@@ -0,0 +1,12 @@
+import MakePost from '@/components/MakePost'
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+        <div className="">
+            <MakePost/>
+        </div>
+    </>
+  )
+}
